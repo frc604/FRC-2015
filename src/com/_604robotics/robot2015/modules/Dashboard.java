84dfe5755ea6563dcb942059621ee4d8eb33755b
Alan Li <alanpusongli@gmail.com>
2015-01-31 13:19:54 -0800

    Add Off option for Xbox rumble, add Dpad test code

--- src/com/_604robotics/robot2015/modules/Dashboard.java
+++ src/com/_604robotics/robot2015/modules/Dashboard.java
@@ -1,10 +1,16 @@
 package com._604robotics.robot2015.modules;
 
+import com._604robotics.robotnik.action.Action;
+import com._604robotics.robotnik.action.ActionData;
+import com._604robotics.robotnik.action.controllers.ElasticController;
 import com._604robotics.robotnik.data.DataMap;
 import com._604robotics.robotnik.data.sources.DashboardData;
 import com._604robotics.robotnik.module.Module;
 import com._604robotics.robotnik.trigger.TriggerMap;
 import com._604robotics.robotnik.trigger.sources.DashboardTriggerChoice;
+import com._604robotics.robotnik.action.field.FieldMap;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Dashboard extends Module {
     public Dashboard () {
@@ -13,10 +19,23 @@ public class Dashboard extends Module {
             add("Tank Drive", driveMode.addDefault("Tank Drive"));
             add("Arcade Drive", driveMode.add("Arcade Drive"));
             add("Stick Drive", driveMode.add("Stick Drive"));
+            add("Off", driveMode.add("Off"));
         }});
         this.set(new DataMap() {{
-    		add("Low Rumble", new DashboardData("Low Rumble", 0));
-    		add("High Rumble", new DashboardData("High Rumble", 0));
     	}});
+        this.set(new ElasticController() {{
+        	addDefault("Resting", new Action() {
+        	});
+        	addDefault("Display", new Action(new FieldMap () {{
+                define("dpad", -1000);
+            }}) {
+        		public void begin (ActionData data) {
+        	    	SmartDashboard.putNumber("Dpad Testing", -1000D);
+        		}
+                public void run (ActionData data) {
+                    SmartDashboard.putNumber("Dpad Testing", data.get("dpad"));
+                }
+            });
+        }});
     }
 }
--- src/com/_604robotics/robotnik/prefabs/controller/ControllerRumble.java
+++ src/com/_604robotics/robotnik/prefabs/controller/ControllerRumble.java
@@ -4,15 +4,27 @@ import com._604robotics.robotnik.data.DataRecipient;
 
 import edu.wpi.first.wpilibj.Joystick;
 
-public class ControllerRumble implements DataRecipient {
+public class ControllerRumble implements DataRecipient{
     private final Joystick joystick;
+    private boolean enable;
+    private float lRumble;
+    private float rRumble;
     
-    public ControllerRumble (Joystick joystick) {
+    public ControllerRumble (final Joystick joystick) {
         this.joystick = joystick;
+        this.enable = false;
+        this.lRumble = 0;
+        this.rRumble = 0;
     }
     
     public void sendData (String fieldName, double dataValue) {
-        if(fieldName == "high rumble") joystick.setRumble(Joystick.RumbleType.kLeftRumble, (float)dataValue);
-        else if(fieldName == "low rumble") joystick.setRumble(Joystick.RumbleType.kRightRumble, (float)dataValue);
+    	if (fieldName == "enable") this.enable = true;
+    	else if (fieldName == "high rumble") this.lRumble = (float) dataValue;
+        else if (fieldName == "low rumble") this.rRumble = (float) dataValue;
+    	
+    	if(this.enable){
+    		this.joystick.setRumble(Joystick.RumbleType.kLeftRumble, lRumble);
+    		this.joystick.setRumble(Joystick.RumbleType.kLeftRumble, rRumble);
+    	}
     }
 }
