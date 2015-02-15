46d9fc135514beac3310006e42996ada181cfdd6
Alan Li <alanpusongli@gmail.com>
2015-02-14 18:44:43 -0800

    Auton mode reverted
    This kills the robot.

--- src/com/_604robotics/robot2015/modes/AutonomousMode.java
+++ src/com/_604robotics/robot2015/modes/AutonomousMode.java
@@ -22,25 +22,6 @@ import edu.wpi.first.wpilibj.Talon;
  * The Class AutonomousMode.
  */
 public class AutonomousMode extends Procedure {
-	Drive drive = new Drive();
-	Encoder rEncoder = new Encoder(3, 4);
-	Encoder lEncoder = new Encoder(2, 1);
-	
-	/** The left. */
-	private final Talon fLeftMotor = new Talon(0);
-	
-	/** The r left. */
-	private final Talon rLeftMotor = new Talon(1);
-	
-	/** The right. */
-	private final Talon fRightMotor = new Talon(2);
-	
-	/** The r right. */
-	private final Talon rRightMotor = new Talon(3);
-    
-    /**
-     * Instantiates a new autonomous mode.
-     */
     public AutonomousMode () {
     }
     
@@ -48,9 +29,5 @@ public class AutonomousMode extends Procedure {
      * @see com._604robotics.robotnik.procedure.Procedure#apply(com._604robotics.robotnik.module.ModuleManager)
      */
     protected void apply (ModuleManager modules) {
-    	while(rEncoder.get()<100){
-    		rLeftMotor.set(1D);
-    		
-    	}
     }
 }
