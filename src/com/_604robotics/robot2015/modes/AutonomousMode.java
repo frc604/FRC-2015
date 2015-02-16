5cf659647393ac1b3c2a4f9f6a51283ca4858889
Alan Li <alanpusongli@gmail.com>
2015-02-15 16:58:14 -0800

    That's not how you auton
    Look at the 2014 Auton; you just bind actions. Needs to be looked at
    though (figure out what stuff means in there).

--- src/com/_604robotics/robot2015/modes/AutonomousMode.java
+++ src/com/_604robotics/robot2015/modes/AutonomousMode.java
@@ -3,27 +3,14 @@
  */
 package com._604robotics.robot2015.modes;
 
-import com._604robotics.robot2015.modules.Drive;
-import com._604robotics.robotnik.coordinator.Coordinator;
-import com._604robotics.robotnik.coordinator.connectors.Binding;
-import com._604robotics.robotnik.coordinator.connectors.DataWire;
 import com._604robotics.robotnik.module.ModuleManager;
-import com._604robotics.robotnik.prefabs.measure.TriggerMeasure;
-import com._604robotics.robotnik.prefabs.trigger.TriggerAnd;
 import com._604robotics.robotnik.procedure.Procedure;
-import com._604robotics.robotnik.procedure.Step;
-import com._604robotics.robotnik.trigger.TriggerAccess;
-
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Talon;
 
 // TODO: Auto-generated Javadoc
 /**
  * The Class AutonomousMode.
  */
 public class AutonomousMode extends Procedure {
-	Drive drive = new Drive();
-    
     /**
      * Instantiates a new autonomous mode.
      */
