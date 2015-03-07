b31fb4a189575dc2f2d2b43a50c7b24a229c4dbf
Alan Li <alanpusongli@gmail.com>
2015-03-06 21:03:29 -0800

    Change Dashboard display to intended method

--- src/com/_604robotics/robot2015/systems/DashboardSystem.java
+++ src/com/_604robotics/robot2015/systems/DashboardSystem.java
@@ -3,6 +3,7 @@ package com._604robotics.robot2015.systems;
 import com._604robotics.robotnik.coordinator.Coordinator;
 import com._604robotics.robotnik.coordinator.connectors.DataWire;
 import com._604robotics.robotnik.module.ModuleManager;
+import com._604robotics.robotnik.prefabs.outputs.DashboardOutput;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -31,28 +32,28 @@ public class DashboardSystem extends Coordinator {
     		this.fill(new DataWire(modules.getModule("Drive").getAction("Servo Drive"),
     				"right clicks", modules.getModule("Dashboard").getData("Right Drive Servo")));
     	}
-    	 this.fill(new DataWire(modules.getModule("Dashboard").getAction("Display"),
-    			 "left clicks", modules.getModule("Drive").getData("Left Drive Clicks")));
-         this.fill(new DataWire(modules.getModule("Dashboard").getAction("Display"),
-        		 "right clicks", modules.getModule("Drive").getData("Right Drive Clicks")));
-    	 this.fill(new DataWire(modules.getModule("Dashboard").getAction("Display"),
-    			 "left rate", modules.getModule("Drive").getData("Left Drive Rate")));
-         this.fill(new DataWire(modules.getModule("Dashboard").getAction("Display"),
-        		 "right rate", modules.getModule("Drive").getData("Right Drive Rate")));
-         this.fill(new DataWire(modules.getModule("Dashboard").getAction("Display"),
-        		 "elevator clicks", modules.getModule("Elevator").getData("Elevator Clicks")));
-         this.fill(new DataWire(modules.getModule("Dashboard").getAction("Display"),
-        		 "elevator rate", modules.getModule("Elevator").getData("Elevator Rate")));
-         this.fill(new DataWire(modules.getModule("Dashboard").getAction("Display"),
-        		 "current gear", modules.getModule("Gear").getData("Gear")));
-         this.fill(new DataWire(modules.getModule("Dashboard").getAction("Display"),
-        		 "current multiplier", modules.getModule("Gear").getData("Current Multiplier")));
+    	 this.fill(new DataWire(DashboardOutput.asDouble(), "left clicks",
+    			 modules.getModule("Drive").getData("Left Drive Clicks")));
+         this.fill(new DataWire(DashboardOutput.asDouble(), "right clicks",
+        		 modules.getModule("Drive").getData("Right Drive Clicks")));
+    	 this.fill(new DataWire(DashboardOutput.asDouble(), "left rate",
+    			 modules.getModule("Drive").getData("Left Drive Rate")));
+         this.fill(new DataWire(DashboardOutput.asDouble(), "right rate",
+        		 modules.getModule("Drive").getData("Right Drive Rate")));
+         this.fill(new DataWire(DashboardOutput.asDouble(), "elevator clicks",
+        		 modules.getModule("Elevator").getData("Elevator Clicks")));
+         this.fill(new DataWire(DashboardOutput.asDouble(), "elevator rate",
+        		 modules.getModule("Elevator").getData("Elevator Rate")));
+         this.fill(new DataWire(DashboardOutput.asDouble(), "current gear",
+        		 modules.getModule("Gear").getData("Gear")));
+         this.fill(new DataWire(DashboardOutput.asDouble(), "current multiplier",
+        		 modules.getModule("Gear").getData("Current Multiplier")));
          
-         this.fill(new DataWire(modules.getModule("Dashboard").getAction("Display"),
-        		 "elevator on target", modules.getModule("Elevator").getTrigger("At Elevator Target")));
-         this.fill(new DataWire(modules.getModule("Dashboard").getAction("Display"),
-        		 "left drive on target", modules.getModule("Drive").getTrigger("At Left Servo Target")));
-         this.fill(new DataWire(modules.getModule("Dashboard").getAction("Display"),
-        		 "right drive on target", modules.getModule("Drive").getTrigger("At Right Servo Target")));
+         this.fill(new DataWire(DashboardOutput.asBoolean(), "elevator on target",
+        		 modules.getModule("Elevator").getTrigger("At Elevator Target")));
+         this.fill(new DataWire(DashboardOutput.asBoolean(), "left drive on target",
+        		 modules.getModule("Drive").getTrigger("At Left Servo Target")));
+         this.fill(new DataWire(DashboardOutput.asBoolean(), "right drive on target",
+        		 modules.getModule("Drive").getTrigger("At Right Servo Target")));
     }
 }
