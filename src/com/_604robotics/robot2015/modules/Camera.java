d5c1401676214712fb343de84fb88ae5b99d331a
L200Classmate <fake_email@annoying.mockito>
2017-02-08 16:18:02 -0800

    Got Camera working

--- src/com/_604robotics/robot2015/modules/Camera.java
+++ src/com/_604robotics/robot2015/modules/Camera.java
@@ -1,12 +1,21 @@
 package com._604robotics.robot2015.modules;
 
+import com._604robotics.robotnik.action.Action;
+import com._604robotics.robotnik.action.ActionData;
+import com._604robotics.robotnik.action.controllers.ElasticController;
 import com._604robotics.robotnik.module.Module;
 
 import edu.wpi.first.wpilibj.CameraServer;
 
 public class Camera extends Module {
-	private static CameraServer Camera;
 	public Camera() {
-		Camera.startAutomaticCapture();
+		CameraServer Camera = CameraServer.getInstance();
+		Camera.startAutomaticCapture("cam1");
+//		this.set(new ElasticController() {{
+//			addDefault("Start", new Action () {
+//				public void begin(ActionData data) {
+//				}
+//			});
+//		}});
 	}
 }
